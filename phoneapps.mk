993acd46abd208f97d6ea30e6d8719c3b9009620
Diorgenes <dyogenes_gd@hotmail.com>
2017-01-01 14:28:24 -0300

    Initial release

---
+++ phoneapps.mk
@@ -0,0 +1,35 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	BasicDreams \
+	Bluetooth \
+	CertInstaller \
+	KeyChain \
+	LatinIME \
+	LiveWallpapers \
+	PacProcessor \
+	PicoTts \
+	PrintSpooler \
+	Stk \
+	telresources \
+	TimeService \
+	WAPPushManager \
+	webview 
+
+private-phone-priv-apps := \
+	AudioFX \
+	BackupRestoreConfirmation \
+	CellBroadcastReceiver \
+	CMAudioService \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	OneTimeInitializer \
+	ProxyHandler \
+	Screencast \
+	SharedStorageBackup \
+	Shell \
+	StatementService 
+	
